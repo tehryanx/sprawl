fcc525e68ceeed9d7007e430f8157ae39fe4479f
Ryan <ryan.cartner@gmail.com>
2020-07-29 12:21:17 -0300

    Update sprawl.py
    Full rewrite.

--- sprawl.py
+++ sprawl.py
@@ -1,60 +1,56 @@
 #!/usr/bin/env python3
 
-import fileinput
+import argparse, textwrap, sys
 
-def expand(path):
+parser = argparse.ArgumentParser(description = "Expand a list of paths into a much larger wordlist by creating an entry for each path depth.")
+
+parser.add_argument('-s', '--slash', default=False, action="store_true", help="prefix each line with a /, this is left off by default.")
+args = parser.parse_args()
+
+prefix = "/" if args.slash else ""
+
+def read_in():
+	# Read contents of stdin
+	return [x.strip() for x in sys.stdin.readlines()]
+
+def strip_extra(line):
+	# Remove protocol handler, domain, querystring
+	line = line[1:] if line[0] == "/" else line
+	line = "/".join(line.split("/")[3:]) if "//" in line else line
+	return line.split("?")[0] if "?" in line else line
+
+def tokenize(line):
+	return line.split("/")
+
+def expand(input_line):
+	# tokenize the line
+	tokens = tokenize(input_line)
+
 	expansion = []
-	path = path.rstrip().rstrip("/")
-	if "://" in path:
-		protocol=path.split("://")[0]
-		path=path.split("://")[1]
-	else:
-		protocol = False
-
-	path_segments = path.split("/")
-	for i in range(0, len(path_segments)):
-		new_path = ""
-		for segment in path_segments:
-			new_path = new_path + segment + "/"
-		if new_path: 
-			if protocol:
-				new_path = protocol + "://" + new_path
-			expansion.append(new_path[0:-1]) 
-		path_segments.pop()
-	path_segments = path.split("/")
-	for i in range(0, len(path_segments)):
-		new_path = ""
-		path_segments.pop(0)
-		for segment in path_segments:
-			new_path = new_path + segment + "/"
-		if new_path:
-			if protocol:
-				new_path = protocol + "://" + new_path
-			expansion.append(new_path[0:-1])
+	for t in tokens:
+		# add each token by itself as a line
+		expansion.append(t)
+	for index in range(len(tokens)):
+		# truncate each path depth and add each result
+		line = "/".join(tokens[0:index+1])
+		expansion.append(line)
+	for index in range(len(tokens)):
+		line = "/".join(tokens[index+1:])
+		expansion.append(line)
 	return expansion
 
-def expandx(url):
-	expansion = []
-	url = url.replace("://", ":||").rstrip().rstrip("/")
-	path_segments = url.split("/")
-	# loop len(path_segments) times, that's how many urls we'll end up with. 
-	for i in range(0, len(path_segments)):
-		new_url = ""
-		for segment in path_segments:
-			new_url = new_url + segment + "/"
-		expansion.append(new_url.replace(":||", "://")[0:-1])
-		path_segments.pop()
-	path_segments = url.split("/")
-	for i in range(0, len(path_segments)):
-		new_url = ""
-		path_segments.pop(0)
-		for segment in path_segments:
-			new_url = new_url + segment + "/"
-		expansion.append(new_url.replace(":||", "://")[0:-1])
-	return expansion
 
-for line in fileinput.input():
-	for i in expand(line):
-		print(i)
+input_data = read_in()
 
+output_set = set()
+
+for line in input_data:
+
+	line = strip_extra(line)
+	expansion = expand(line)
+	for entry in expansion:
+		output_set.add(f"{prefix}{entry}")
+
+for entry in output_set:
+	print(entry)
 
