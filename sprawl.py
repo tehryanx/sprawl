09d4819aefa4375a888d24c7bc8a382d7a107efd
Ryan <ryan.cartner@gmail.com>
2020-07-29 12:21:46 -0300

    Update sprawl.py
    Removed unused lib

--- sprawl.py
+++ sprawl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, textwrap, sys
+import argparse, sys
 
 parser = argparse.ArgumentParser(description = "Expand a list of paths into a much larger wordlist by creating an entry for each path depth.")
 
